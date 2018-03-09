3722a143dc4e2c88d8a5405584d17f5b6a09a3a4
Milber Champutiz Burbano <milber@alturasoluciones.com>
2018-03-09 11:38:28 -0500

    add Logo in Login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Login.css';
+import Logo from '../Logo/Logo'
+
+export default class Login extends Component {
+  render() {
+    return (
+      <Logo/>
+     )
+  }
+}
