bd85ac721709de1710cb68441b0fe04cc218b6f9
Alberto Aragon <alberto@alturasoluciones.com>
2018-03-09 11:54:30 -0500

    Adding Firebase to the App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from "./components/Login/Login";
+import config from './config'
+import firebase from 'firebase';
 
 class App extends Component {
   render() {
---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  configFirebase: {
+    apiKey: "AIzaSyD7jl619qdLTDMsF-OHSv55GNJgDaAxhyY",
+    authDomain: "askan-6ccad.firebaseapp.com",
+    databaseURL: "https://askan-6ccad.firebaseio.com",
+    projectId: "askan-6ccad",
+    storageBucket: "askan-6ccad.appspot.com",
+    messagingSenderId: "799773169786"
+  }
+};
+
+export default config;
