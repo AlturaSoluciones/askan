65d57a6b5d9de04b2aaaf06a375594e16a7c5fc9
Alberto Aragon <alberto@alturasoluciones.com>
2018-03-09 13:04:15 -0500

    Commenting Redux logic to avoid warnings and errors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+// import { createStore, applyMiddleware, combineReducers } from 'redux';
+// import { Provider } from 'react-redux';
+// import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import * as reducers from './store/reducers';
-const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
+// import * as reducers from './store/reducers';
+// const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  <App/>,
   document.getElementById('root')
 );
 registerServiceWorker();
