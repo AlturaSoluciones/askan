4c0218c5f38b53ce40b1dff8c7f2797bc9db6baa
Alberto Aragon <alberto@alturasoluciones.com>
2018-06-22 01:53:39 -0500

    Adding Boards and Deleting Boards

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -14,9 +14,14 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import AddIcon from '@material-ui/icons/Add';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export default class Dashboard extends Component {
   constructor(props) {
@@ -24,14 +29,19 @@ export default class Dashboard extends Component {
     this.state = {
       email: '',
       password: '',
+      boards: [
+        {
+          name: 'Board 1'
+        },
+        {
+          name: 'Board 2'
+        }
+      ],
+      new_board_name: ''
     };
     this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
   }
 
-  handleTextFieldChange(event) {
-    const name = event.target.name;
-    this.setState({[name]: event.target.value});
-  }
 
   handleLogout(toggleAuth, setUid) {
     auth.doSignOut()
@@ -46,33 +56,71 @@ export default class Dashboard extends Component {
       })
   }
 
-  renderRedirect(){
-    this.props.history.push(routes.LANDING);
-  }
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  handleEdit = (key) => {
+    console.log("edited: ", key);
+  };
+
+  handleDelete = (key) => {
+    this.setState({boards: this.state.boards.filter(board => board.name !== key)});
+  };
 
   render() {
     return (
-        <AuthConsumer>
+
+<AuthConsumer>
           {({ isAuth, toggleAuth, setUid }) => (
 
-            <div className="dashboardContainer">
-              DASHBOARD TEST
+      <div className="root">
+{isAuth ? (
+        <AppBar position="static">
+          <Toolbar>
+            <Logo/>
+            <Button color="inherit" onClick={() => this.handleLogout()}>Logout</Button>
+          </Toolbar>
+        </AppBar>
+        <div className="add-board">
+          <TextField
+            id="add-board-text"
+            label="Board Name"
+            className="textField"
+            value={this.state.new_board_name}
+            onChange={this.handleChange('new_board_name')}
+            margin="normal"
+          />
+          <Button variant="fab" color="primary" aria-label="add" className="add-board-button" onClick={this.addBoard}>
+            <AddIcon/>
+          </Button>
+        </div>
 
-              {isAuth ? (
-                <div>
-                 <Logo/>
-                  <Link to='#' className="logout" onClick={ () => this.handleLogout(toggleAuth, setUid) }>
-                    Logout
-                  </Link>
-                </div>
-              ) : (
-                <div>{this.renderRedirect()}</div>
+        <div className="boards-list">
+          <List component="nav">
+            {this.state.boards.map(board => {
+              return (
+                <ListItem key={board.name}>
+                  <ListItemText primary={board.name}/>
+                  <IconButton aria-label="Edit" onClick={ () => this.handleEdit(board.name) }>
+                    <EditIcon/>
+                  </IconButton>
+                  <IconButton>
+                    <DeleteIcon onClick={ () => this.handleDelete(board.name) }/>
+                  </IconButton>
+                </ListItem>
+              );
+            })}
+          </List>
+        </div>
+) : (
+<div>{this.renderRedirect()}</div>
               )}
-
-            </div>
-
-            )}
+      </div>
+)}
         </AuthConsumer>
-     )
+    )
   }
 }
